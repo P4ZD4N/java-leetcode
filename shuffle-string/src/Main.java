a3e40e881c8b41ba5df703218942bc13a2094b71
Wiktor <wiko700@gmail.com>
2023-08-27 21:05:49 +0200

    Added solution of problem "1528. Shuffle String"

---
+++ shuffle-string/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.restoreString("codeleet", new int[] {4, 5, 6, 7, 0, 2, 1, 3}));
+        System.out.println(solution.restoreString("abc", new int[] {0, 1, 2}));
+        System.out.println(solution.restoreString("rtoikw", new int[] {5, 3, 4, 1, 2, 0}));
+    }
+}
---
+++ shuffle-string/src/Solution.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.List;
+
+class Solution {
+    public String restoreString(String s, int[] indices) {
+        String[] splittedString = s.split("");
+        List<String> shuffledString = Arrays.asList(Arrays.copyOf(splittedString, s.length()));
+
+        for (int i = 0; i < indices.length; i++) {
+            shuffledString.set(indices[i], splittedString[i]);
+        }
+
+        return String.join("", shuffledString);
+    }
+}
