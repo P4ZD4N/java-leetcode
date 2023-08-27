f484fef46174a3af42ce0ac8d38be8e158fabd5b
Wiktor <wiko700@gmail.com>
2023-08-27 21:17:59 +0200

    Modified length parameter in copyOf method

--- shuffle-string/src/Solution.java
+++ shuffle-string/src/Solution.java
@@ -4,7 +4,7 @@ import java.util.List;
 class Solution {
     public String restoreString(String s, int[] indices) {
         String[] splittedString = s.split("");
-        List<String> shuffledString = Arrays.asList(Arrays.copyOf(splittedString, s.length()));
+        List<String> shuffledString = Arrays.asList(Arrays.copyOf(splittedString, splittedString.length));
 
         for (int i = 0; i < indices.length; i++) {
             shuffledString.set(indices[i], splittedString[i]);
