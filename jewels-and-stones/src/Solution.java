468210f54174fe5142b6667191962daa487f7555
Wiktor <wiko700@gmail.com>
2023-08-29 21:33:10 +0200

    Added method, witch checks if all characters in jewels string are unique

--- jewels-and-stones/src/Solution.java
+++ jewels-and-stones/src/Solution.java
@@ -1,21 +1,43 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
     public int numJewelsInStones(String jewels, String stones) {
         if (jewels.matches("[a-zA-Z]+") && stones.matches("[a-zA-Z]+")) {
-            String[] charsInJewelsStr = jewels.split("");
-            String[] charsInStonesStr = stones.split("");
+            if (areAllCharactersInJewelsStringUnique(jewels)) {
+                String[] charsInJewelsStr = jewels.split("");
+                String[] charsInStonesStr = stones.split("");
 
-            int jewelsCounter = 0;
+                int jewelsCounter = 0;
 
-            for (String charInJewels : charsInJewelsStr) {
-                for (String charInStones : charsInStonesStr) {
-                    if (charInJewels.equals(charInStones)) {
-                        jewelsCounter++;
+                for (String charInJewels : charsInJewelsStr) {
+                    for (String charInStones : charsInStonesStr) {
+                        if (charInJewels.equals(charInStones)) {
+                            jewelsCounter++;
+                        }
                     }
                 }
+                return jewelsCounter;
+            } else {
+                throw new IllegalArgumentException("All the characters of jewels must be unique");
             }
-            return jewelsCounter;
         } else {
             throw new IllegalArgumentException("Both strings must consists of only English letters");
         }
+    }
+    private boolean areAllCharactersInJewelsStringUnique(String jewels) {
+        List<String> charactersInString = new ArrayList<>();
+        boolean areAllCharactersInJewelsStringUnique = true;
+
+        for (int i = 0; i < jewels.length(); i++) {
+            if (charactersInString.contains(Character.toString(jewels.charAt(i)))) {
+                areAllCharactersInJewelsStringUnique = false;
+                break;
+            } else {
+                charactersInString.add(Character.toString(jewels.charAt(i)));
+            }
+        }
+
+        return areAllCharactersInJewelsStringUnique;
     }
 }
