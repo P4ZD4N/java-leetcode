a78001deedcf2642d970a73aeb02ebd79088d409
Wiktor <wiko700@gmail.com>
2023-10-17 20:12:44 +0200

    Implemented constraints

--- happy-number/src/Solution.java
+++ happy-number/src/Solution.java
@@ -2,14 +2,21 @@ import java.util.List;
 
 class Solution {
     public boolean isHappy(int n) {
-        int result = n;
+        if (checkWhetherNumberRespectsConstraints(n)) {
+            int result = n;
 
-        while (result != 1 && result != 4){
-            List<String> digits = List.of(Integer.toString(result).split(""));
-            int sum = digits.stream().mapToInt(digit -> (int) Math.pow(Integer.parseInt(digit), 2)).sum();
-            result = sum;
+            while (result != 1 && result != 4) {
+                List<String> digits = List.of(Integer.toString(result).split(""));
+                int sum = digits.stream().mapToInt(digit -> (int) Math.pow(Integer.parseInt(digit), 2)).sum();
+                result = sum;
+            }
+
+            return result == 1;
+        } else {
+            throw new IllegalArgumentException("Incorrect number");
         }
-
-        return result == 1;
+    }
+    public boolean checkWhetherNumberRespectsConstraints(int n) {
+        return n >= 1 && (int) (Math.pow(2, 31) - 1) >= n;
     }
 }
