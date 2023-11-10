df0c42ef9685a0f49ac90cb77db37d82d620117b
Wiktor <wiko700@gmail.com>
2023-11-10 17:30:50 +0100

    Added more unit tests

--- keyboard-row/src/test/java/SolutionTest.java
+++ keyboard-row/src/test/java/SolutionTest.java
@@ -7,6 +7,45 @@ class SolutionTest {
 
     @Test
     void resultShouldBeAlaskaAndDad() {
-        assertEquals(new String[] {"Alaska", "Dad"}, s.findWords(new String[] {"Hello","Alaska","Dad","Peace"}));
+        assertArrayEquals(new String[] {"Alaska", "Dad"}, s.findWords(new String[] {"Hello","Alaska","Dad","Peace"}));
+    }
+
+    @Test
+    void resultShouldBeEmptyArray() {
+        assertArrayEquals(new String[] {}, s.findWords(new String[] {"omk"}));
+    }
+
+    @Test
+    void resultShouldBeTheSameArrayWhichIsEnteredAsInput() {
+        assertArrayEquals(new String[] {"adsdf","sfd"}, s.findWords(new String[] {"adsdf","sfd"}));
+    }
+
+    @Test
+    void resultShouldBeQwertyAndDfg() {
+        assertArrayEquals(new String[] {"QWERTY", "DfG"}, s.findWords(new String[] {"Hi","QWERTY","hI","DfG", "nmlkf"}));
+    }
+
+    @Test
+    void arrayEnteredAsInputShouldThrowIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    s.findWords(new String[21]);
+                });
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    s.findWords(new String[0]);
+                });
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    s.findWords(new String[] {"łódź", "boat"});
+                });
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    s.findWords(new String[] {"123", "456"});
+                });
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    s.findWords(new String[] {".", ","});
+                });
     }
 }
