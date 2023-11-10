236a0460b5ca0078fe01622c82c9c02acdb9cf07
Wiktor <wiko700@gmail.com>
2023-11-10 17:30:00 +0100

    Added unit tests

--- keyboard-row/src/test/java/SolutionTest.java
+++ keyboard-row/src/test/java/SolutionTest.java
@@ -3,5 +3,10 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SolutionTest {
+    Solution s = new Solution();
+
     @Test
+    void resultShouldBeAlaskaAndDad() {
+        assertEquals(new String[] {"Alaska", "Dad"}, s.findWords(new String[] {"Hello","Alaska","Dad","Peace"}));
+    }
 }
