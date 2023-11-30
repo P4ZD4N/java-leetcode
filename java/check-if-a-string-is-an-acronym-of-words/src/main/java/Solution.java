1cdc695efb8eefe9d160937a89291e0b1a5a2789
Wiktor <wiko700@gmail.com>
2023-11-30 01:36:49 +0100

    Implemented constraints and changed variables names to make code more obvious

--- java/check-if-a-string-is-an-acronym-of-words/src/main/java/Solution.java
+++ java/check-if-a-string-is-an-acronym-of-words/src/main/java/Solution.java
@@ -2,15 +2,44 @@ import java.util.List;
 
 public class Solution {
     public boolean isAcronym(List<String> words, String s) {
-        if (words.size() == s.length()) {
-            String[] letters = s.split("");
-            boolean result = true;
-            for (int i = 0; i < words.size() && result; i++)
-                if (!(letters[i].equals(words.get(i).substring(0,1))))
-                    result = false;
-            return result;
-        } else {
-            return false;
-        }
+        if (
+                listHasCorrectSize(words) &&
+                allElementsOfListHaveCorrectSize(words) &&
+                acronymHasCorrectSize(s) &&
+                allElementsOfListAndAcronymConsistOfLowercaseEnglishLetters(words, s)
+        ) {
+            if (words.size() == s.length()) {
+                String[] acronymLetters = s.split("");
+                boolean isAcronym = true;
+                for (int i = 0; i < words.size() && isAcronym; i++)
+                    if (!(acronymLetters[i].equals(words.get(i).substring(0, 1))))
+                        isAcronym = false;
+                return isAcronym;
+            } else
+                return false;
+        } else
+            throw new IllegalArgumentException();
+    }
+    private boolean listHasCorrectSize(List<String> words) {
+        return words.size() >= 1 && 100 >= words.size();
+    }
+    private boolean allElementsOfListHaveCorrectSize(List<String> words) {
+        boolean allElementsOfListHaveCorrectSize = true;
+        for (String word : words)
+            if (!(word.length() >= 1 && 10 >= word.length()))
+                allElementsOfListHaveCorrectSize = false;
+        return allElementsOfListHaveCorrectSize;
+    }
+    private boolean acronymHasCorrectSize(String acronym) {
+        return acronym.length() >= 1 && 100 >= acronym.length();
+    }
+    private boolean allElementsOfListAndAcronymConsistOfLowercaseEnglishLetters(List<String> words, String acronym) {
+        boolean allElementsOfListAndAcronymConsistOfLowercaseEnglishLetters = true;
+        for (String word : words)
+            if (!(word.matches("[a-z]+")))
+                allElementsOfListAndAcronymConsistOfLowercaseEnglishLetters = false;
+        if (!(acronym.matches("[a-z]+")))
+            allElementsOfListAndAcronymConsistOfLowercaseEnglishLetters = false;
+        return allElementsOfListAndAcronymConsistOfLowercaseEnglishLetters;
     }
 }
