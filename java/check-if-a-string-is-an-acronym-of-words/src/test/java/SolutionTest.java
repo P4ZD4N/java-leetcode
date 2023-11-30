0edd18f4763fe4f032c7393b166257052da8837c
Wiktor <wiko700@gmail.com>
2023-11-30 01:37:40 +0100

    Added unit tests of constraints

--- java/check-if-a-string-is-an-acronym-of-words/src/test/java/SolutionTest.java
+++ java/check-if-a-string-is-an-acronym-of-words/src/test/java/SolutionTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -16,5 +17,109 @@ class SolutionTest {
     @Test
     void shouldReturnFalse() {
         assertEquals(false, solution.isAcronym(List.of("an", "apple"), "a"));
+    }
+
+    @Test
+    void sizeOfListGreaterThan100ShouldThrowIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    solution.isAcronym(new ArrayList<>(100), "abc");
+                });
+    }
+
+    @Test
+    void sizeOfListLessThan1ShouldThrowIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    solution.isAcronym(new ArrayList<>(0), "bca");
+                });
+    }
+
+    @Test
+    void lengthOfAnyElementOfListGreaterThan10ShouldThrowIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    solution.isAcronym(List.of("a".repeat(11)), "a");
+                });
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    solution.isAcronym(List.of("aa","a".repeat(11)), "a");
+                });
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    solution.isAcronym(List.of("a".repeat(11), "aa"), "a");
+                });
+    }
+
+    @Test
+    void lengthOfAnyElementOfListLessThan1ShouldThrowIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    solution.isAcronym(List.of("","a".repeat(5)), "a");
+                });
+    }
+
+    @Test
+    void lengthOfAcronymGreaterThan100ShouldThrowIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    solution.isAcronym(List.of("aa","a"), "a".repeat(101));
+                });
+    }
+
+    @Test
+    void lengthOfAcronymLessThan1ShouldThrowIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    solution.isAcronym(List.of("aa","a"), "");
+                });
+    }
+
+    @Test
+    void anyElementOfListContainingUppercaseLettersShouldThrowIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    solution.isAcronym(List.of("Aa", "b"), "a");
+                });
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    solution.isAcronym(List.of("AA", "BB"), "a");
+                });
+    }
+
+    @Test
+    void anyElementOfListContainingCharacterOutOfEnglishAlphabetShouldThrowIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    solution.isAcronym(List.of("śa", "b"), "a");
+                });
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    solution.isAcronym(List.of("8", "nn"), "a");
+                });
+    }
+
+    @Test
+    void acronymContainingUppercaseLettersShouldThrowIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    solution.isAcronym(List.of("aa", "b"), "A");
+                });
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    solution.isAcronym(List.of("aa", "bb"), "Aa");
+                });
+    }
+
+    @Test
+    void acronymContainingCharacterOutOfEnglishAlphabetShouldThrowIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    solution.isAcronym(List.of("sa", "b"), "ś");
+                });
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    solution.isAcronym(List.of("aaf", "nn"), "8");
+                });
     }
 }
