01bbfc40a605fed9680af8126161254a1a068665
Wiktor <wiko700@gmail.com>
2023-12-01 01:03:37 +0100

    Implemented constraints and refactored some code

--- java/height-checker/src/main/java/Solution.java
+++ java/height-checker/src/main/java/Solution.java
@@ -2,15 +2,34 @@ import java.util.Arrays;
 
 public class Solution {
     public int heightChecker(int[] heights) {
-        int[] sortedHeights = Arrays.copyOf(heights, heights.length);
-        Arrays.sort(sortedHeights);
-        return findNumberOfIncorrectStandingStudents(heights, sortedHeights);
+        if (hasArrayCorrectLength(heights) && areAllElementsBelongToCorrectRange(heights)) {
+            int[] sortedHeights = Arrays.copyOf(heights, heights.length);
+            Arrays.sort(sortedHeights);
+            return findNumberOfIncorrectStandingStudents(heights, sortedHeights);
+        } else {
+            throw new IllegalArgumentException();
+        }
+
     }
     private int findNumberOfIncorrectStandingStudents(int[] firstArray, int[] secondArray) {
         int numberOfIncorrectStandingStudents = 0;
-        for (int i = 0; i < firstArray.length; i++)
-            if (firstArray[i] != secondArray[i])
+        for (int i = 0; i < firstArray.length; i++) {
+            if (firstArray[i] != secondArray[i]) {
                 numberOfIncorrectStandingStudents++;
+            }
+        }
         return numberOfIncorrectStandingStudents;
+    }
+    private boolean hasArrayCorrectLength(int[] array) {
+        return array.length >= 1 && 100 >= array.length;
+    }
+    private boolean areAllElementsBelongToCorrectRange(int[] array) {
+        boolean areAllElementsBelongToCorrectRange = true;
+        for (int i = 0; i < array.length && areAllElementsBelongToCorrectRange; i++) {
+            if (!(array[i] >= 1 && 100 >= array[i])) {
+                areAllElementsBelongToCorrectRange = false;
+            }
+        }
+        return areAllElementsBelongToCorrectRange;
     }
 }
