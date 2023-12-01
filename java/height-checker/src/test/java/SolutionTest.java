e1e8e59765f483b6f4ab64419b9d27557e46965f
Wiktor <wiko700@gmail.com>
2023-12-01 01:04:57 +0100

    Added unit tests of constraints

--- java/height-checker/src/test/java/SolutionTest.java
+++ java/height-checker/src/test/java/SolutionTest.java
@@ -19,4 +19,36 @@ class SolutionTest {
     void thisArrayShouldReturn0() {
         assertEquals(0, solution.heightChecker(new int[] {1,2,3,4,5}));
     }
+
+    @Test
+    void lengthGreaterThan100ShouldThrowIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    solution.heightChecker(new int[101]);
+                });
+    }
+
+    @Test
+    void lengthSmallerThan1ShouldThrowIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    solution.heightChecker(new int[0]);
+                });
+    }
+
+    @Test
+    void anyElementGreaterThan100ShouldThrowIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    solution.heightChecker(new int[] {1,101,2});
+                });
+    }
+
+    @Test
+    void anyElementSmallerThan1ShouldThrowIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    solution.heightChecker(new int[] {1,0,2});
+                });
+    }
 }
