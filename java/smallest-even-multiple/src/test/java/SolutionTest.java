cb83a4d7e8cd926fbf723e95ed5485f921fa1cae
Wiktor <wiko700@gmail.com>
2024-01-02 22:13:37 +0100

    Added tests of constraints and renamed other tests

--- java/smallest-even-multiple/src/test/java/SolutionTest.java
+++ java/smallest-even-multiple/src/test/java/SolutionTest.java
@@ -6,17 +6,33 @@ class SolutionTest {
     Solution solution = new Solution();
 
     @Test
-    void fiveAndTwoShouldReturn10() {
+    void fiveShouldReturn10() {
         assertEquals(10, solution.smallestEvenMultiple(5));
     }
 
     @Test
-    void sixAndTwoShouldReturn6() {
+    void sixShouldReturn6() {
         assertEquals(6, solution.smallestEvenMultiple(6));
     }
 
     @Test
-    void sevenAndTwoShouldReturn14() {
+    void sevenShouldReturn14() {
         assertEquals(14, solution.smallestEvenMultiple(7));
+    }
+
+    @Test
+    void zeroAndTwoShouldThrowIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    solution.smallestEvenMultiple(0);
+                });
+    }
+
+    @Test
+    void oneHundredFiftyOneShouldThrowIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    solution.smallestEvenMultiple(151);
+                });
     }
 }
