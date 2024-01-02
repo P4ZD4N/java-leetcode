3ce1d2a9313d13fc233516fd7c811a287a343b05
Wiktor <wiko700@gmail.com>
2024-01-02 22:12:36 +0100

    Implemented constraints

--- java/smallest-even-multiple/src/main/java/Solution.java
+++ java/smallest-even-multiple/src/main/java/Solution.java
@@ -1,6 +1,8 @@
 public class Solution {
     public int smallestEvenMultiple(int n) {
-        if (n % 2 == 0) return n;
-        else return n * 2;
+        if (n >= 1 && 150 >= n) {
+            if (n % 2 == 0) return n;
+            else return n * 2;
+        } else throw new IllegalArgumentException("Number too big or too small!");
     }
 }
