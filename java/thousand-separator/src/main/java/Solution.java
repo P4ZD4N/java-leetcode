6b2b888722efec339c1811ad1fb016ad78135f11
Wiktor <wiko700@gmail.com>
2024-02-25 01:30:30 +0100

    Refactored code

--- java/thousand-separator/src/main/java/Solution.java
+++ java/thousand-separator/src/main/java/Solution.java
@@ -1,28 +1,67 @@
 public class Solution {
-    public String thousandSeparator(int n) {
-        int digitsInNumber = Integer.toString(n).length();
-        if (3 >= digitsInNumber && n >= 0)
-            return Integer.toString(n);
-        else if (digitsInNumber >= 4 && n > 0) {
-            String[] digits = Integer.toString(n).split("");
-            StringBuilder numberWithDots = new StringBuilder();
-            int dots = digits.length % 3 == 0 ? (digits.length / 3) - 1 : digits.length / 3;
+    public String thousandSeparator(int number) {
+        int numberOfDigitsInNumber = getNumberOfDigitsInNumber(number);
 
-            int dotDetector = 1;
-            int nextDigitFromBehind = digits.length - 1;
-            for (int i = digits.length + dots - 1; i >= 0; i--) {
-                if (dotDetector % 4 == 0 && i > 0) {
-                    numberWithDots.append(".");
-                    dotDetector = 1;
-                } else {
-                    numberWithDots.append(digits[nextDigitFromBehind]);
-                    dotDetector++;
-                    nextDigitFromBehind--;
-                }
+        if (isNumberOfDigitsLessThanOrEqualToThree(numberOfDigitsInNumber) && isNumberGraterThanOrEqualToZero(number))
+            return getIntConvertedToString(number);
+        else if (isNumberOfDigitsGreaterThanOrEqualToFour(numberOfDigitsInNumber) && isNumberGraterThanZero(number)) {
+            String[] digitsInNumber = getIntConvertedToString(number).split("");
+            return getNumberWithDots(digitsInNumber);
+        } else
+            throw new IllegalArgumentException("Illegal argument");
+    }
+
+    private int getNumberOfDigitsInNumber(int number) {
+        return getIntConvertedToString(number).length();
+    }
+
+    private String getIntConvertedToString(int number) {
+        return Integer.toString(number);
+    }
+
+    private boolean isNumberOfDigitsLessThanOrEqualToThree(int numberOfDigitsInNumber) {
+        return 3 >= numberOfDigitsInNumber;
+    }
+
+    private boolean isNumberGraterThanOrEqualToZero(int number) {
+        return number >= 0;
+    }
+
+    private boolean isNumberOfDigitsGreaterThanOrEqualToFour(int numberOfDigitsInNumber) {
+        return numberOfDigitsInNumber >= 4;
+    }
+
+    private boolean isNumberGraterThanZero(int number) {
+        return number > 0;
+    }
+
+    private String getNumberWithDots(String[] digitsInNumber) {
+        StringBuilder numberWithDots = new StringBuilder();
+        int numberOfDigitsInNumber = digitsInNumber.length;
+        int numberOfDots = getNumberOfDots(digitsInNumber);
+
+        int dotDetector = 1;
+        int nextDigitFromBehind = numberOfDigitsInNumber - 1;
+
+        for (int i = numberOfDigitsInNumber + numberOfDots - 1; i >= 0; i--) {
+            if (isDotDetectorDivisibleByFour(dotDetector) && isNumberGraterThanZero(i)) {
+                numberWithDots.append(".");
+                dotDetector = 1;
+            } else {
+                numberWithDots.append(digitsInNumber[nextDigitFromBehind]);
+                dotDetector++;
+                nextDigitFromBehind--;
             }
+        }
 
-            return numberWithDots.reverse().toString();
+        return numberWithDots.reverse().toString();
+    }
 
-        } else throw new IllegalArgumentException("Illegal argument");
+    private int getNumberOfDots(String[] digitsInNumber) {
+        return digitsInNumber.length % 3 == 0 ? (digitsInNumber.length / 3) - 1 : digitsInNumber.length / 3;
+    }
+
+    private boolean isDotDetectorDivisibleByFour(int dotDetector) {
+        return dotDetector % 4 == 0;
     }
 }
